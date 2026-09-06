a6ab844f10007114041c8174c97f8bd6167ceed2
mhasan-exe <mikailfardan221@gmail.com>
2026-09-06 21:03:12 +0500

    Pagination

--- main.py
+++ main.py
@@ -20,3 +20,10 @@ posts: list[dict] = [{
 @app.get("/")
 def homepage(request: Request):
     return templates.TemplateResponse(request, "index.html", {"posts": posts, "answer": "This is a sample answer."})
+
+@app.get("/posts/{post_id}")
+def get_post(request: Request, post_id: int):
+    post = next((post for post in posts if post["id"] == post_id), None)
+    if not post:
+        return {"error": "Post not found"}
+    return templates.TemplateResponse(request, "post.html", {"post": post})
